f3225901be2126ef17fb7b7748c929d851cb5e86
Nassim SALHI <nassimsalhi@nassims-macbook-pro.home>
2023-03-15 21:48:41 +0100

    push

--- src/main.c
+++ src/main.c
@@ -6,18 +6,21 @@
 /*   By: nassimsalhi <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 15:48:53 by nassimsalhi       #+#    #+#             */
-/*   Updated: 2023/03/15 16:15:57 by nassimsalhi      ###   ########.fr       */
+/*   Updated: 2023/03/15 17:05:29 by nassimsalhi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
+//Main function of the progrann
 int	main(int ac, char **av)
 {
+	t_struct	pipex;
+
 	if (ac != 5)
 		ft_printf("Wrong number of argument");
-	else
-	{
-
-	}
+//recuperer le fd
+	pipex.infile = open(av[1], O_RDONLY);
+	if (pipex.infile < 0)
+		ft_printf("ERROR WITH THE FILE");
 }
