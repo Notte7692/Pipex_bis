087458d66caaa5df1b9fd44d7ddc693a414c0919
Nassim SALHI <nassimsalhi@nassims-macbook-pro.home>
2023-04-07 09:32:06 +0200

    push

--- src/child.c
+++ src/child.c
@@ -6,7 +6,7 @@
 /*   By: nsalhi <nsalhi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 11:06:07 by nsalhi            #+#    #+#             */
-/*   Updated: 2023/03/29 16:19:08 by nsalhi           ###   ########.fr       */
+/*   Updated: 2023/04/06 20:55:53 by nassimsalhi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,9 @@ static char	*get_cmd(char **cmd_path, char *cmd)
 {
 	char	*tmp;
 	char	*command;
-	
+
 	if (access(cmd, 0) == 0)
 		return (cmd);
-	
 	while (*cmd_path)
 	{
 		tmp = ft_strjoin(*cmd_path, "/");
@@ -33,9 +32,7 @@ static char	*get_cmd(char **cmd_path, char *cmd)
 	return (NULL);
 }
 
-
-
-void    first_child(t_struct pipex, char **av, char **envp)
+void	first_child(t_struct pipex, char **av, char **envp)
 {
 	dup2(pipex.pipefd[1], 1);
 	close(pipex.pipefd[0]);
@@ -59,7 +56,7 @@ void    first_child(t_struct pipex, char **av, char **envp)
 
 void	second_child(t_struct pipex, char **av, char **envp)
 {
-	dup2(pipex.pipefd[0],0);
+	dup2(pipex.pipefd[0], 0);
 	close(pipex.pipefd[1]);
 	dup2(pipex.outfile, 1);
 	close(pipex.outfile);
