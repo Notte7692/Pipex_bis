6904fd2ef1583cadebcb1a4b909aa0603e1460ca
Nassim <nsalhi@student.42.fr>
2023-04-10 13:49:06 +0200

    push

--- bonus/pipex_bonus_bis.c
+++ bonus/pipex_bonus_bis.c
@@ -184,12 +184,18 @@ int main(int ac, char **av, char **envp)
     char        **commands;
     t_pipex     cmd;
     int         i;
+	int			fd;
     int         current_pipe[2];
 	int			previous_pipe[2];
 	int			*pids;
 
-	if (open(av[1], O_RDONLY) < 0 || !ft_strncmp("here_doc", av[1], 9))
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0 || !ft_strncmp("here_doc", av[1], 9))
+	{
+		close(fd);
 		return (0);
+	}
+	close(fd);
     commands = parse_command(ac, av);
     if (commands == NULL)
 	{
