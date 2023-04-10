75de6b4ff6b8171b67ca4dd943381cdeba9e2dd8
Nassim <nsalhi@student.42.fr>
2023-04-10 16:40:19 +0200

    push

--- bonus/pipex_bonus_bis.c
+++ bonus/pipex_bonus_bis.c
@@ -190,7 +190,7 @@ int main(int ac, char **av, char **envp)
 	int			*pids;
 
 	fd = open(av[1], O_RDONLY);
-	if (fd < 0 || !ft_strncmp("here_doc", av[1], 9))
+	if (!ft_strncmp("here_doc", av[1], 9) || fd < 0)
 	{
 		close(fd);
 		return (0);
